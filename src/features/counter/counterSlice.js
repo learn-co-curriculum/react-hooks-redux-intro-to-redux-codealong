f70af1eb74b008dba2a82ef8a4c4143871db4cfc
ihollander <ianholla@gmail.com>
2020-12-08 09:29:56 -0500

    Added solution

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -5,11 +5,14 @@ const initialState = {
 function counterReducer(state = initialState, action) {
   switch (action.type) {
     case "count/increment":
+      console.log("Current state.items length %s", state.items.length);
+      console.log("Updating state.items length to %s", state.items.length + 1);
       return {
         ...state,
         items: state.items.concat(state.items.length + 1),
       };
     default:
+      console.log("Initial state.items length %s", state.items.length);
       return state;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux"; /* code change */
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); /* code change */
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
